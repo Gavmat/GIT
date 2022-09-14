41342db21182dea41a5d9447a07ec89c437d3cef
Gavmat <gavrmat@yandex.ru>
2022-09-14 10:56:58 +0300

    new files

--- react-employee-accounting/src/components/app-filter/app-filter.js
+++ react-employee-accounting/src/components/app-filter/app-filter.js
@@ -1,21 +1,62 @@
 import "./app-filter.css";
 
-const AppFilter = () => {
+
+// В этом компоненте не будет никакого состояния, потмоу что кнопка никакого value не содержит. На нее нажали,
+//  она что то сделала, а эти действия мы передадим через пропсы.
+// Когда кнопок много, особенно фильтров, они к нам приходят не в качестве верстки, а чаще всего расположены
+// в каком то массиве данных.Так удобнее работать.Когда кнопки формируются из каких то данных.
+
+
+
+const AppFilter = (props) => {
+    // сформируем кнопки.
+    const buttonsData = [
+        { name: 'all', label: ' Все сотрудники' },
+        { name: 'rise', label: ' На повышение' },
+        { name: 'moreThen1000', label: '  З/П больше 1000$' },
+    ];
+    // на базе этих данных, мы можем сформировать массив элементов.
+
+    const buttons = buttonsData.map(({ name, label }) => {
+        // Создаем переменную в которой мы определяем активный элемент или нет. Те - где совпадает фильтр
+        const active = props.filter === name;
+        // Эта строчка заменяет условие что if props.filter = name, который приходит,  то в таком случае мы 
+        // возвращаем true в active. В итоге в переменной active может быть true либо false. Оно один раз будет true
+        // Теперь это значение используем чтобы сформировать класс. 
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+
+        return (
+            <button type="button"
+                className={`btn ${clazz}`}
+                key={name}
+                onClick={() => props.onFilterSelect(name)}
+            >
+                {label}
+            </button>
+
+        )
+    })
+
+
     return (
         <div className="btn-group">
-            <button type="button"
-                    className="btn btn-light">
-                    Все сотрудники
-            </button>
-            <button type="button"
-                    className="btn btn-outline-light">
-                    На повышение
-            </button>
-            <button type="button"
-                    className="btn btn-outline-light">
-                    З/П больше 1000$
-            </button>
-        </div>
+            {buttons}
+        </div >
+
+        // <div className="btn-group">
+        //     <button type="button"
+        //         className="btn btn-light">
+        //         Все сотрудники
+        //     </button>
+        //     <button type="button"
+        //         className="btn btn-outline-light">
+        //         На повышение
+        //     </button>
+        //     <button type="button"
+        //         className="btn btn-outline-light">
+        //         З/П больше 1000$
+        //     </button>
+        // </div>
     )
 }
 
--- react-employee-accounting/src/components/search-panel/search-panel.js
+++ react-employee-accounting/src/components/search-panel/search-panel.js
@@ -1,11 +1,42 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
-        <input type="text"
+
+
+// Можно сразу вытащить из пропсов метод onUpdateSearch и повесить на onChange, но мы сделаем так
+// чтобы компонент был управляемый - чтобы input нормально синхронизировался с локальным стейтом
+// те будем не только передавать стейт наверх но и содержать внутри компонента. для этого создаем
+// метод который назовем так же.
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+        }
+    }
+
+    onUpdateSearch = (e) => {
+        // создаем переменную которая будет получаться из event.target.value.Те когда срабатывает
+        // событие мы получаем value которое ввел пользователь.
+        const term = e.target.value;
+        // устанавливаем сетстейт локального характера
+        this.setState({ term });
+        // теперь пробрасываем это состояние наверх. Делаем это при помощи проперти 
+        // который к нам пришел и туда просто передаем строчку term,те то что ввел user 
+        this.props.onUpdateSearch(term);
+        // эти функции хоть и называются одинаково, но одна работает локально, а вторая приходит из другого
+        // компонента и выполняет свою роль
+    }
+
+    render() {
+        return (
+            <input type="text"
                 className="form-control search-input"
-                placeholder="Найти сотрудника"/>
-    )
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearch} />
+        )
+    }
 }
 
 export default SearchPanel;
